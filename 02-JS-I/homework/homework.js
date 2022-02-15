a1b5a76d6ab0de5bd6a4a274f6c554038f762200
bryanrfz <bryan_rfz@hotmail.com>
2022-02-14 20:52:11 -0500

    Commit Bool JS1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Empezando el Curso";
 const nuevoNum = 25;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
